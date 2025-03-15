01c3d49ae934b0e17d5ca47bea023bafc4ff1951
Kalinga Jayathilaka <kalingajayathilaka@gmail.com>
2025-03-15 11:35:12 +0530

    implement submission model class

---
+++ code-assessment-module/backend/app/models/submission_model.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+
+
+class Submission:
+    def __init__(self, submission_id, student_id, status, assignment_id, code_id=None, report_id=None, video_id=None):
+        self.submission_id = submission_id
+        self.student_id = student_id  # Document ID reference to User
+        self.assignment_id = assignment_id
+        self.status = status
+        self.code_id = code_id
+        self.report_id = report_id
+        self.video_id = video_id
+        self.created_at = datetime.utcnow().isoformat()
+
+    def to_dict(self):
+        return {
+            "submission_id": self.submission_id,
+            "student_id": self.student_id,
+            "assignment_id": self.assignment_id,
+            "status": self.status,
+            "code_id": self.code_id,
+            "report_id": self.report_id,
+            "video_id": self.video_id,
+            "created_at": self.created_at
+        }
+
+    def save(self, db):
+        db.collection("submissions").document(self.submission_id).set(self.to_dict())
