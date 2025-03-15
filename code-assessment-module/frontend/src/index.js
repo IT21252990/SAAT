6f3542c8c3b914e7e5054f7f6e781237799228fe
Kalinga Jayathilaka <kalingajayathilaka@gmail.com>
2025-03-15 11:42:23 +0530

    update index.js file by adding new routes

--- code-assessment-module/frontend/src/index.js
+++ code-assessment-module/frontend/src/index.js
@@ -1,12 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import App from "./App";
+import Login from "./pages/user/Login";
+import Register from "./pages/user/Register";
+import StudentHome from "./pages/student/StudentHome";
+import TeacherHome from "./pages/teacher/TeacherHome";
+import ModulePage from "./pages/student/ModulePage";
+import AssignmentPage from "./pages/student/AssignmentPage";
+import AddSubmission from "./pages/AddSubmission";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Create root for React 18+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/student-home" element={<StudentHome />} />
+        <Route path="/teacher-home" element={<TeacherHome />} />
+        <Route path="/module-page/:moduleId" element={<ModulePage />} />
+        <Route path="/assignment/:assignmentId" element={<AssignmentPage />} />
+        <Route path="/submission/:submissionId" element={<AddSubmission/>} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
-
