843b287d7a1c808d1cfa47069a9bfedb6f2e38b3
Kalinga Jayathilaka <kalingajayathilaka@gmail.com>
2025-03-16 18:48:29 +0530

    Integrating the project

---
+++ Smart-Assignment-Assessment-Tool/frontend/src/pages/user/Register.jsx
@@ -0,0 +1,159 @@
+import React, { useState } from "react";
+import { auth } from "../../firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+import { DarkThemeToggle } from "flowbite-react";
+import saat_logo from "../../asserts/rounded_logo.png"
+
+const Register = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [role, setRole] = useState("student");
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    setError(null);
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password,
+      );
+      const user = userCredential.user;
+
+      // Save role in Firestore using Flask API
+      await fetch("http://127.0.0.1:5000/user/saveUser", {
+        method: "POST",
+        body: JSON.stringify({ uid: user.uid, email: user.email, role }),
+        headers: { "Content-Type": "application/json" },
+      });
+
+      // Redirect based on role
+      navigate(role === "student" ? "/student-home" : "/teacher-home");
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  return (
+    <section className="bg-gray-50 dark:bg-gray-900">
+        <DarkThemeToggle className="absolute right-4 top-4" />
+      <div className="mx-auto flex flex-col items-center justify-center px-6 py-8 md:h-screen lg:py-0">
+        <a
+          href="#"
+          className="mb-6 flex items-center text-2xl font-semibold text-gray-900 dark:text-white"
+        >
+          <img
+            className="mr-2 h-8 w-8"
+            src={saat_logo}
+            alt="logo"
+          />
+          &nbsp;&nbsp;Welcome !
+        </a>
+        <div className="w-full rounded-lg bg-white shadow dark:border dark:border-gray-700 dark:bg-gray-800 sm:max-w-md md:mt-0 xl:p-0">
+          <div className="space-y-4 p-6 sm:p-8 md:space-y-6">
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 dark:text-white md:text-2xl">
+              Create an account
+            </h1>
+            <form className="space-y-4 md:space-y-6" onSubmit={handleRegister}>
+              <div>
+                <label
+                  for="email"
+                  className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Your email
+                </label>
+                <input
+                  type="email"
+                  onChange={(e) => setEmail(e.target.value)}
+                  name="email"
+                  id="email"
+                  className="focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+                  placeholder="example@example.com"
+                  required=""
+                />
+              </div>
+              <div>
+                <label
+                  for="password"
+                  className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Password
+                </label>
+                <input
+                  type="password"
+                  onChange={(e) => setPassword(e.target.value)}
+                  name="password"
+                  id="password"
+                  placeholder="••••••••"
+                  class="focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+                  required=""
+                />
+              </div>
+              <div>
+                <label
+                  for="user_role"
+                  class="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Select your Role
+                </label>
+                <select
+                  id="user_role"
+                  class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+                >
+                  <option selected value="student">Student</option>
+                  <option value="teacher">Teacher</option>
+                </select>
+              </div>
+              <div className="flex items-start">
+                <div className="flex h-5 items-center">
+                  <input
+                    id="terms"
+                    aria-describedby="terms"
+                    type="checkbox"
+                    className="focus:ring-3 focus:ring-primary-300 dark:focus:ring-primary-600 h-4 w-4 rounded border border-gray-300 bg-gray-50 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800"
+                    required=""
+                  />
+                </div>
+                <div className="ml-3 text-sm">
+                  <label
+                    for="terms"
+                    className="font-light text-gray-500 dark:text-gray-300"
+                  >
+                    I accept the{" "}
+                    <a
+                      className="text-primary-600 dark:text-primary-500 font-medium hover:underline"
+                      href="#"
+                    >
+                      Terms and Conditions
+                    </a>
+                  </label>
+                </div>
+              </div>
+              {error && <p style={{ color: "red" }}>{error}</p>}
+              <button
+                type="submit"
+                className="bg-primary-600 hover:bg-primary-700 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 w-full rounded-lg px-5 py-2.5 text-center text-sm font-medium text-white focus:outline-none focus:ring-4"
+              >
+                Create an account
+              </button>
+              <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                Already have an account?{" "}
+                <a
+                  onClick={() => navigate("/login")}
+                  className="text-primary-600 cursor-pointer dark:text-primary-500 font-medium hover:underline"
+                >
+                  Login here
+                </a>
+              </p>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Register;
