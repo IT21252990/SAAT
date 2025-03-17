d1cdf5ea581afe9b254562345d217e1884a7a5f2
Kalinga Jayathilaka <kalingajayathilaka@gmail.com>
2025-03-17 15:04:30 +0530

    update view code submission page to contributor commitis

--- Smart-Assignment-Assessment-Tool/frontend/src/pages/teacher/ViewCodeSubmission.jsx
+++ Smart-Assignment-Assessment-Tool/frontend/src/pages/teacher/ViewCodeSubmission.jsx
@@ -1,12 +1,19 @@
 import React, { useState } from "react";
-import { useNavigate , useLocation  } from "react-router-dom"; 
-import { FaSitemap, FaSearch, FaCodeBranch, FaPuzzlePiece, FaHome } from "react-icons/fa";
+import { useNavigate, useLocation } from "react-router-dom";
+import {
+  FaSitemap,
+  FaSearch,
+  FaCodeBranch,
+  FaPuzzlePiece,
+  FaHome,
+} from "react-icons/fa";
 import { MdFeedback } from "react-icons/md";
 import { FaFileSignature } from "react-icons/fa6";
 import { RiAccountPinCircleFill } from "react-icons/ri";
-import RepoExplorer from "../../components/RepoExplorer"; 
+import RepoExplorer from "../../components/RepoExplorer";
 import LocalFileExplorer from "../../components/LocalFileExplorer";
 import ContributorCommitHistory from "../../components/ContributorCommitHistory";
+import Header from "../../components/Header";
 
 const ViewCodeSubmission = () => {
   const [activeSection, setActiveSection] = useState("explorer");
@@ -16,97 +23,86 @@ const ViewCodeSubmission = () => {
 
   const renderActiveSection = () => {
     switch (activeSection) {
-      case "explorer":{
-         if(githubUrl){
-          return <RepoExplorer repoUrl={githubUrl}/> 
-        }if(!githubUrl){
-        //   return <LocalFileExplorer/>
+      case "explorer": {
+        if (githubUrl) {
+          return <RepoExplorer repoUrl={githubUrl} />;
+        }
+        if (!githubUrl) {
+          //   return <LocalFileExplorer/>
         }
       }
-      case "search":
-        return <div>search section</div>
-      case "sourceControl":
-        return <ContributorCommitHistory repoUrl={repoUrl} />
+      case "contributions":
+        return <ContributorCommitHistory repoUrl={githubUrl} />;
       case "extensions":
-        return <div>search section</div>
+        return <div>search section</div>;
       default:
         return null;
     }
   };
 
   const handleHomeClick = () => {
-    navigate("/students-submissions"); // Navigate to the students submission page
-  };
-
-  const handleProfileClick = () => {
-    navigate("/profile"); // Navigate to the profile page
+    navigate(-1);
   };
 
   return (
-    <div style={{ display: "flex", height: "100vh" }}>
-      {/* Sidebar */}
-      <div
-        style={{
-          width: "60px",
-          backgroundColor: "#1e1e1e",
-          color: "#fff",
-          marginRight:"10px",
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          paddingTop: "10px",
-          paddingRight:"10px",
-          justifyContent: "space-between", // Push content to the top and bottom
-          height: "100%", // Ensure it spans full height
-        }}
-      >
-        <div>
-          <SidebarIcon
-            icon={<FaSitemap size={24}/>}
-            label="Explorer"
-            active={activeSection === "explorer"}
-            onClick={() => setActiveSection("explorer")}
-          />
-          <SidebarIcon
-            icon={<FaSearch size={24}/>}
-            label="Search"
-            active={activeSection === "search"}
-            onClick={() => setActiveSection("search")}
-          />
-          <SidebarIcon
-            icon={<FaFileSignature size={24}/>}
-            label="Source Control"
-            active={activeSection === "sourceControl"}
-            onClick={() => setActiveSection("sourceControl")}
-          />
-          <SidebarIcon
-            icon={<MdFeedback size={24}/>}
-            label="Extensions"
-            active={activeSection === "extensions"}
-            onClick={() => setActiveSection("extensions")}
-          />
-        </div>
+    <div className="flex h-screen max-w-full flex-col">
+      <div className="flex-none">
+        <Header />
+      </div>
+      <div className="flex-grow bg-gray-50 dark:bg-gray-900">
+        <div style={{ display: "flex", height: "100vh" }}>
+          {/* Sidebar */}
+          <div
+            style={{
+              width: "60px",
+              backgroundColor: "#1e1e1e",
+              color: "#fff",
+              marginRight: "10px",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              paddingTop: "10px",
+              paddingRight: "10px",
+              justifyContent: "space-between", // Push content to the top and bottom
+              height: "100%", // Ensure it spans full height
+            }}
+          >
+            <div>
+              <SidebarIcon
+                icon={<FaSitemap size={24} />}
+                label="Explorer"
+                active={activeSection === "explorer"}
+                onClick={() => setActiveSection("explorer")}
+              />
+              <SidebarIcon
+                icon={<FaFileSignature size={24} />}
+                label="Contributions"
+                active={activeSection === "contributions"}
+                onClick={() => setActiveSection("contributions")}
+              />
+              <SidebarIcon
+                icon={<MdFeedback size={24} />}
+                label="Extensions"
+                active={activeSection === "extensions"}
+                onClick={() => setActiveSection("extensions")}
+              />
+            </div>
 
-        <div>
-          {/* Home Icon */}
-          <SidebarIcon
-            icon={<FaHome size={24}/>}
-            label="Home"
-            active={activeSection === "Home"}
-            onClick={handleHomeClick}
-          />
-          {/* Profile Icon */}
-          <SidebarIcon
-            icon={<RiAccountPinCircleFill size={24}/>}
-            label="Profile"
-            active={activeSection === "profile"}
-            onClick={handleProfileClick}
-          />
+            <div>
+              {/* Home Icon */}
+              <SidebarIcon
+                icon={<FaHome size={24} />}
+                label="Back to Students Submissions"
+                active={activeSection === "Home"}
+                onClick={handleHomeClick}
+              />
+            </div>
+          </div>
+
+          {/* Main Content */}
+          <div style={{ flex: 1 }}>{renderActiveSection()}</div>
         </div>
       </div>
-
-      {/* Main Content */}
-      <div style={{ flex: 1 }}>{renderActiveSection()}</div>
     </div>
   );
 };
