3daeb69849ae7242214f1b13f1d111bb81fe4389
Kalinga Jayathilaka <kalingajayathilaka@gmail.com>
2025-05-24 13:33:24 +0530

    Update model to add all 3 types marking schemes

--- Smart-Assignment-Assessment-Tool/backend/app/models/markingScheme.py
+++ Smart-Assignment-Assessment-Tool/backend/app/models/markingScheme.py
@@ -1,5 +1,5 @@
 class MarkingScheme:
-    def __init__(self, user_id, module_code, start_date, due_date, status, title, criteria, assignment_id):
+    def __init__(self, user_id, module_code, start_date, due_date, status, title, criteria, assignment_id, submission_types=None, submission_type_weights=None):
         self.user_id = user_id  # User ID
         self.module_code = module_code
         self.start_date = start_date
@@ -7,18 +7,22 @@ class MarkingScheme:
         self.status = status
         self.assignment_id = assignment_id
         self.title = title
-        self.criteria = criteria  # List of dictionaries containing criterion details
+        self.criteria = criteria  # Dict of marking criteria for each submission type
+        self.submission_types = submission_types or {} 
+        self.submission_type_weights = submission_type_weights or {}
 
     def to_dict(self):
         return {
-            "user_id": self.user_id,  # Added user_id to the dictionary
+            "user_id": self.user_id,  # user_id to the dictionary
             "module_code": self.module_code,
             "start_date": self.start_date,
             "due_date": self.due_date,
             "status": self.status,
             "assignment_id": self.assignment_id,
             "title": self.title,
-            "criteria": self.criteria,  # Example: [{'criterion': 'Accuracy', 'weightage': 20, 'subCriteria': [{'description': 'Correctness of algorithm', 'points': 10}]}]
+            "criteria": self.criteria,  # Example: {"code": [{}], "report": [{}], "video": [{}]}
+            "submission_types": self.submission_types,
+            "submission_type_weights": self.submission_type_weights
         }
 
     def save(self, db):
